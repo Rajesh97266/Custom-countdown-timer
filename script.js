9c7cad48bc2002c5ffcb941e11c8a19f506e41b6
Rajesh97266 <rajesh97266@gmail.com>
2024-11-23 11:51:26 +0530

    form-added

--- script.js
+++ script.js
@@ -11,5 +11,21 @@ const completeEl = document.getElementById("complete");
 const completeElInfo = document.getElementById("complete-info");
 const completeBtn = document.getElementById("complete-button");
 
+let countdownTitle = "";
+let countdownDate = "";
+
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
+
+//Take values from form
+function updateForm(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  console.log(e);
+  console.log(countdownTitle);
+  console.log(countdownDate);
+}
+
+//Event Listener
+countdownForm.addEventListener("submit", updateForm);
